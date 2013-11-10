1065800878605ad97613915a4760074aeb672c8b
Rian Hunter <rian@alum.mit.edu>
2013-11-09 16:51:40 -0800

    don't include .hg directory when checking if dependencies are up to date

--- are_dependencies_up_to_date.sh
+++ are_dependencies_up_to_date.sh
@@ -12,7 +12,9 @@ fi
 # TODO: check headers as well!
 
 ANYGLOG_FILE=$(find "$APPDIR/../google-glog" \
+    -type f -and \
     -newer "$APPDIR/out/deps/lib/libglog.a" | \
+    grep -v "^$APPDIR/../google-glog/.hg/" | \
     head -n 1 2>/dev/null)
 if [ ! -e "$APPDIR/out/deps/lib/libglog.a" ] || [ "$ANYGLOG_FILE" ]; then
     echo "google-glog is out of date" >> /dev/stderr
@@ -20,6 +22,7 @@ if [ ! -e "$APPDIR/out/deps/lib/libglog.a" ] || [ "$ANYGLOG_FILE" ]; then
 fi
 
 ANYBOTAN_FILE=$(find "$APPDIR/encfs-dependencies/botan" \
+    -type f -and \
     -newer "$APPDIR/out/deps/lib/libbotan-1.10.a" | \
     head -n 1 2>/dev/null)
 if [ ! -e "$APPDIR/out/deps/lib/libbotan-1.10.a" ] || [ "$ANYBOTAN_FILE" ]; then
@@ -28,7 +31,9 @@ if [ ! -e "$APPDIR/out/deps/lib/libbotan-1.10.a" ] || [ "$ANYBOTAN_FILE" ]; then
 fi
 
 ANYPROTOBUF_FILE=$(find "$APPDIR/../protobuf" \
+    -type f -and \
     -newer "$APPDIR/out/deps/lib/libprotobuf.a" | \
+    grep -v "^$APPDIR/../protobuf/.hg/" | \
     head -n 1 2>/dev/null)
 if [ ! -e "$APPDIR/out/deps/lib/libprotobuf.a" ] || [ "$ANYPROTOBUF" ]; then
     echo "protobuf is out of date" >> /dev/stderr
@@ -36,6 +41,7 @@ if [ ! -e "$APPDIR/out/deps/lib/libprotobuf.a" ] || [ "$ANYPROTOBUF" ]; then
 fi
 
 ANYTINYXML_FILE=$(find "$APPDIR/encfs-dependencies/tinyxml" \
+    -type f -and \
     -newer "$APPDIR/out/deps/lib/libtinyxml.a" | \
     head -n 1 2>/dev/null)
 if [ ! -e "$APPDIR/out/deps/lib/libtinyxml.a" ] || [ "$ANYTINYXML_FILE" ]; then
@@ -44,7 +50,9 @@ if [ ! -e "$APPDIR/out/deps/lib/libtinyxml.a" ] || [ "$ANYTINYXML_FILE" ]; then
 fi
 
 ANYENCFS_FILE=$(find "$APPDIR/../encfs" \
+    -type f -and \
     -newer "$APPDIR/out/deps/lib/libencfs.a" | \
+    grep -v "^$APPDIR/../encfs/.hg/" | \
     head -n 1 2>/dev/null)
 if [ ! -e "$APPDIR/out/deps/lib/libencfs.a" ] || [ "$ANYENCFS_FILE" ]; then
     echo "encfs is out of date" >> /dev/stderr
@@ -52,7 +60,9 @@ if [ ! -e "$APPDIR/out/deps/lib/libencfs.a" ] || [ "$ANYENCFS_FILE" ]; then
 fi
 
 ANYWEBDAV_SERVER_FS_FILE=$(find "$APPDIR/../davfuse" \
+    -type f -and \
     -newer "$APPDIR/out/deps/lib/libwebdav_server_fs.a" | \
+    grep -v "^$APPDIR/../encfs/.hg/" | \
     head -n 1 2>/dev/null)
 if [ ! -e "$APPDIR/out/deps/lib/libwebdav_server_fs.a" ] || [ "$ANYWEBDAV_SERVER_FS_FILE" ]; then
     echo "davfuse is out of date" >> /dev/stderr
