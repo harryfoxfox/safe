81a82c3ae03454430d96b320d970be9423122f23
Rian Hunter <rian@alum.mit.edu>
2013-11-15 14:50:19 -0800

    fix objective-c memory leaks in unicode_fs_mac.mm

--- src/lockbox/unicode_fs_mac.mm
+++ src/lockbox/unicode_fs_mac.mm
@@ -22,9 +22,11 @@
 
 namespace lockbox { namespace unicode_fs { namespace mac {
 
+// following ARC-guidelines, this method should be have the
+// substring "create" or "copy" in its name
 static
 NSString *
-utf8_nsstring_from_str(const std::string & comp) {
+create_utf8_nsstring_from_str(const std::string & comp) {
   NSData *data = [NSData dataWithBytesNoCopy:(void *)comp.data()
                                       length:comp.size()
                                 freeWhenDone:NO];
@@ -34,36 +36,44 @@ utf8_nsstring_from_str(const std::string & comp) {
 
 std::string
 normalize_path_component_for_fs(const std::string & comp) {
-  NSString *str = utf8_nsstring_from_str(comp);
-  if (!str) throw std::runtime_error("invalid utf8");
-  NSString *nfc_str = str.precomposedStringWithCanonicalMapping;
-  return std::string(nfc_str.UTF8String);
+  @autoreleasepool {
+    NSString *str = create_utf8_nsstring_from_str(comp);
+    if (!str) throw std::runtime_error("invalid utf8");
+    NSString *nfc_str = str.precomposedStringWithCanonicalMapping;
+    return std::string(nfc_str.UTF8String);
+  }
 }
 
 std::string
 normalize_path_component_for_user(const std::string & comp) {
-  NSString *str = utf8_nsstring_from_str(comp);
-  if (!str) throw std::runtime_error("invalid utf8");
-  NSString *nfc_str = str.decomposedStringWithCanonicalMapping;
-  return std::string(nfc_str.UTF8String);
+  @autoreleasepool {
+    NSString *str = create_utf8_nsstring_from_str(comp);
+    if (!str) throw std::runtime_error("invalid utf8");
+    NSString *nfc_str = str.decomposedStringWithCanonicalMapping;
+    return std::string(nfc_str.UTF8String);
+  }
 }
 
 bool
 is_normalized_path_component(const std::string & comp) {
-  NSString *str = utf8_nsstring_from_str(comp);
-  if (!str) return false;
-  return [str isEqualToString:str.precomposedStringWithCanonicalMapping];
+  @autoreleasepool {
+    NSString *str = create_utf8_nsstring_from_str(comp);
+    if (!str) return false;
+    return [str isEqualToString:str.precomposedStringWithCanonicalMapping];
+  }
 }
 
 bool
 normalized_path_components_equal(const std::string & comp_a,
                                  const std::string & comp_b) {
-  assert(is_normalized_path_component(comp_a));
-  assert(is_normalized_path_component(comp_b));
-  NSString *str_a = utf8_nsstring_from_str(comp_a);
-  NSString *str_b = utf8_nsstring_from_str(comp_b);
-  return [str_a compare:str_b
-                options:(NSCaseInsensitiveSearch | NSLiteralSearch)] == NSOrderedSame;
+  @autoreleasepool {
+    assert(is_normalized_path_component(comp_a));
+    assert(is_normalized_path_component(comp_b));
+    NSString *str_a = create_utf8_nsstring_from_str(comp_a);
+    NSString *str_b = create_utf8_nsstring_from_str(comp_b);
+    return [str_a compare:str_b
+                  options:(NSCaseInsensitiveSearch | NSLiteralSearch)] == NSOrderedSame;
+  }
 }
 
 }}}
