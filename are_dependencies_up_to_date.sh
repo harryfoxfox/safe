e6b5bb2fb4f89ed6cab427ee73e3f622b832eba2
Rian Hunter <rian@alum.mit.edu>
2013-11-21 22:43:51 -0800

    actually filter out stderr when files don't exist in are_dependencies_up_to_date.sh

--- are_dependencies_up_to_date.sh
+++ are_dependencies_up_to_date.sh
@@ -13,9 +13,9 @@ fi
 
 ANYGLOG_FILE=$(find "$APPDIR/../google-glog" \
     -type f -and \
-    -newer "$APPDIR/out/deps/lib/libglog.a" | \
+    -newer "$APPDIR/out/deps/lib/libglog.a" 2>/dev/null | \
     grep -v "^$APPDIR/../google-glog/.hg/" | \
-    head -n 1 2>/dev/null)
+    head -n 1)
 if [ ! -e "$APPDIR/out/deps/lib/libglog.a" ] || [ "$ANYGLOG_FILE" ]; then
     echo "google-glog is out of date" >> /dev/stderr
     exit $DEPS_OUT_OF_DATE_RETURN_CODE
@@ -23,8 +23,8 @@ fi
 
 ANYBOTAN_FILE=$(find "$APPDIR/encfs-dependencies/botan" \
     -type f -and \
-    -newer "$APPDIR/out/deps/lib/libbotan-1.10.a" | \
-    head -n 1 2>/dev/null)
+    -newer "$APPDIR/out/deps/lib/libbotan-1.10.a" 2>/dev/null | \
+    head -n 1)
 if [ ! -e "$APPDIR/out/deps/lib/libbotan-1.10.a" ] || [ "$ANYBOTAN_FILE" ]; then
     echo "botan is out of date" >> /dev/stderr
     exit $DEPS_OUT_OF_DATE_RETURN_CODE
@@ -32,9 +32,9 @@ fi
 
 ANYPROTOBUF_FILE=$(find "$APPDIR/../protobuf" \
     -type f -and \
-    -newer "$APPDIR/out/deps/lib/libprotobuf.a" | \
+    -newer "$APPDIR/out/deps/lib/libprotobuf.a" 2>/dev/null | \
     grep -v "^$APPDIR/../protobuf/.hg/" | \
-    head -n 1 2>/dev/null)
+    head -n 1)
 if [ ! -e "$APPDIR/out/deps/lib/libprotobuf.a" ] || [ "$ANYPROTOBUF" ]; then
     echo "protobuf is out of date" >> /dev/stderr
     exit $DEPS_OUT_OF_DATE_RETURN_CODE
@@ -42,8 +42,8 @@ fi
 
 ANYTINYXML_FILE=$(find "$APPDIR/encfs-dependencies/tinyxml" \
     -type f -and \
-    -newer "$APPDIR/out/deps/lib/libtinyxml.a" | \
-    head -n 1 2>/dev/null)
+    -newer "$APPDIR/out/deps/lib/libtinyxml.a" 2>/dev/null | \
+    head -n 1)
 if [ ! -e "$APPDIR/out/deps/lib/libtinyxml.a" ] || [ "$ANYTINYXML_FILE" ]; then
     echo "tinyxml is out of date" >> /dev/stderr
     exit $DEPS_OUT_OF_DATE_RETURN_CODE
@@ -51,9 +51,9 @@ fi
 
 ANYENCFS_FILE=$(find "$APPDIR/../encfs" \
     -type f -and \
-    -newer "$APPDIR/out/deps/lib/libencfs.a" | \
+    -newer "$APPDIR/out/deps/lib/libencfs.a" 2>/dev/null | \
     grep -v "^$APPDIR/../encfs/.hg/" | \
-    head -n 1 2>/dev/null)
+    head -n 1)
 if [ ! -e "$APPDIR/out/deps/lib/libencfs.a" ] || [ "$ANYENCFS_FILE" ]; then
     echo "encfs is out of date" >> /dev/stderr
     exit $DEPS_OUT_OF_DATE_RETURN_CODE
@@ -61,9 +61,9 @@ fi
 
 ANYWEBDAV_SERVER_FS_FILE=$(find "$APPDIR/../davfuse" \
     -type f -and \
-    -newer "$APPDIR/out/deps/lib/libwebdav_server_fs.a" | \
+    -newer "$APPDIR/out/deps/lib/libwebdav_server_fs.a" 2>/dev/null | \
     grep -v "^$APPDIR/../davfuse/.hg/" | \
-    head -n 1 2>/dev/null)
+    head -n 1)
 if [ ! -e "$APPDIR/out/deps/lib/libwebdav_server_fs.a" ] || [ "$ANYWEBDAV_SERVER_FS_FILE" ]; then
     echo "davfuse is out of date" >> /dev/stderr
     exit $DEPS_OUT_OF_DATE_RETURN_CODE
