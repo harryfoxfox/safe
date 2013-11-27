1c1fdc131e1120b6c6a5f1465b68f56942ad89f3
Rian Hunter <rian@alum.mit.edu>
2013-11-26 22:33:46 -0800

    add windows_app_actions.{cpp,hpp}
    --HG--
    branch : intermediate-windows

---
+++ src/lockbox/windows_app_actions.hpp
@@ -0,0 +1,31 @@
+/*
+  Lockbox: Encrypted File System
+  Copyright (C) 2013 Rian Hunter <rian@alum.mit.edu>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#ifndef __windows_app_actions_hpp
+#define __windows_app_actions_hpp
+
+#include <lockbox/lean_windows.h>
+
+namespace lockbox { namespace win {
+
+bool
+open_src_code(HWND owner);
+
+}}
+
+#endif
