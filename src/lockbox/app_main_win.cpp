7391a6e45217f12deb020d75b90de0993a809fcb
Rian Hunter <rian@alum.mit.edu>
2013-12-08 05:33:25 -0800

    open new mount when mounted on windows

--- src/lockbox/app_main_win.cpp
+++ src/lockbox/app_main_win.cpp
@@ -200,6 +200,7 @@ new_mount(WindowData & wd, lockbox::win::MountDetails md) {
   wd.recent_mount_paths_store.use_path(md.get_source_path());
   wd.mounts.push_back(std::move(md));
   update_tray_menu(wd);
+  wd.mounts.back().open_mount();
 }
 
 static
