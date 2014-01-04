05b94076860e6e1649cdd4628c3049d826a24d88
Rian Hunter <rian@alum.mit.edu>
2014-01-04 12:48:37 -0800

    generate icns file from svg's instead of checking pngs into repo
    --HG--
    extra : amend_source : 99e556193dc95bf5475fa4fad831eb4b9f2addb8

--- create_icns.sh
+++ create_icns.sh
@@ -1,19 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 DIR=$(mktemp -d -t createicns)
 
-cp logo-16-color.png "$DIR/icon_16x16.png"
-cp logo-32.png "$DIR/icon_16x16@2x.png"
-cp logo-32.png "$DIR/icon_32x32.png"
-cp logo-64.png "$DIR/icon_32x32@2x.png"
-cp logo-128.png "$DIR/icon_128x128.png"
-cp logo-256.png "$DIR/icon_128x128@2x.png"
-cp logo-256.png "$DIR/icon_256x256.png"
-cp logo-512.png "$DIR/icon_256x256@2x.png"
-cp logo-512.png "$DIR/icon_512x512.png"
-cp logo-1024.png "$DIR/icon_512x512@2x.png"
+PATH_TO_INKSCAPE=inkscape
+
+if ! which "$PATH_TO_INKSCAPE" > /dev/null && [ -d /Applications/Inkscape.app ]; then
+    # guessing on mac
+    PATH_TO_INKSCAPE="/Applications/Inkscape.app/Contents/Resources/bin/inkscape"
+fi
+
+function to_png () {
+    "$PATH_TO_INKSCAPE" "$1" "--export-width=$2" "--export-height=$2" "--export-png=$3"
+}
+
+to_png logo-16-color.svg 16 "$DIR/icon_16x16.png"
+to_png logo-16-color.svg 32 "$DIR/icon_16x16@2x.png"
+to_png logo-32.svg 32 "$DIR/icon_32x32.png"
+to_png logo-128.svg 64 "$DIR/icon_32x32@2x.png"
+to_png logo-128.svg 128 "$DIR/icon_128x128.png"
+to_png logo-128.svg 256 "$DIR/icon_128x128@2x.png"
+to_png logo-128.svg 256 "$DIR/icon_256x256.png"
+to_png logo-128.svg 512 "$DIR/icon_256x256@2x.png"
+to_png logo-128.svg 512 "$DIR/icon_512x512.png"
+to_png logo-128.svg 1024 "$DIR/icon_512x512@2x.png"
 
 if which pngcrush > /dev/null; then
     cd "$DIR"
