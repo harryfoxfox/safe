9537ace58ef9f1d7d231746a82a38b3870708b53
Rian Hunter <rian@alum.mit.edu>
2014-01-03 16:44:30 -0800

    add high resolution icons for mac

--- create_icns.sh
+++ create_icns.sh
@@ -9,6 +9,9 @@ cp logo-32.png "$DIR/icon_32x32.png"
 cp logo-48.png "$DIR/icon_48x48.png"
 cp logo-64.png "$DIR/icon_32x32@2x.png"
 cp logo-128.png "$DIR/icon_128x128.png"
+cp logo-256.png "$DIR/icon_256x256.png"
+cp logo-512.png "$DIR/icon_512x512.png"
+cp logo-1024.png "$DIR/icon_512x512@2x.png"
 
 mv "$DIR" "$DIR.iconset"
 
