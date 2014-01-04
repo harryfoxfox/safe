3ada1f7912f0e4cd95a6874cb153d74aaf5ef8cc
Rian Hunter <rian@alum.mit.edu>
2014-01-03 16:14:52 -0800

    add create_icns.sh script

---
+++ create_icns.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+DIR=$(mktemp -d -t createicns)
+
+cp logo-16-color.png "$DIR/icon_16x16.png"
+cp logo-32.png "$DIR/icon_32x32.png"
+cp logo-48.png "$DIR/icon_48x48.png"
+cp logo-128.png "$DIR/icon_128x128.png"
+
+mv "$DIR" "$DIR.iconset"
+
+iconutil -c icns "$DIR.iconset"
+
+mv "$DIR.icns" "output.icns"
