4e4088f8e21584b42f33973b4136ea32095f7ef0
Rian Hunter <rian@alum.mit.edu>
2014-01-04 13:12:31 -0800

    create_icns.sh: Add option to create Mac menu bar icon.

--- create_icns.sh
+++ create_icns.sh
@@ -15,16 +15,21 @@ function to_png () {
     "$PATH_TO_INKSCAPE" "$1" "--export-width=$2" "--export-height=$2" "--export-png=$3"
 }
 
-to_png logo-16-color.svg 16 "$DIR/icon_16x16.png"
-to_png logo-16-color.svg 32 "$DIR/icon_16x16@2x.png"
-to_png logo-32.svg 32 "$DIR/icon_32x32.png"
-to_png logo-128.svg 64 "$DIR/icon_32x32@2x.png"
-to_png logo-128.svg 128 "$DIR/icon_128x128.png"
-to_png logo-128.svg 256 "$DIR/icon_128x128@2x.png"
-to_png logo-128.svg 256 "$DIR/icon_256x256.png"
-to_png logo-128.svg 512 "$DIR/icon_256x256@2x.png"
-to_png logo-128.svg 512 "$DIR/icon_512x512.png"
-to_png logo-128.svg 1024 "$DIR/icon_512x512@2x.png"
+if [ "$1" = menuBarIcon ]; then 
+    to_png logo-16-bw.svg 16 "$DIR/icon_16x16.png"
+    to_png logo-32-bw.svg 32 "$DIR/icon_16x16@2x.png"
+else
+    to_png logo-16-color.svg 16 "$DIR/icon_16x16.png"
+    to_png logo-16-color.svg 32 "$DIR/icon_16x16@2x.png"
+    to_png logo-32.svg 32 "$DIR/icon_32x32.png"
+    to_png logo-128.svg 64 "$DIR/icon_32x32@2x.png"
+    to_png logo-128.svg 128 "$DIR/icon_128x128.png"
+    to_png logo-128.svg 256 "$DIR/icon_128x128@2x.png"
+    to_png logo-128.svg 256 "$DIR/icon_256x256.png"
+    to_png logo-128.svg 512 "$DIR/icon_256x256@2x.png"
+    to_png logo-128.svg 512 "$DIR/icon_512x512.png"
+    to_png logo-128.svg 1024 "$DIR/icon_512x512@2x.png"
+fi
 
 if which pngcrush > /dev/null; then
     cd "$DIR"
