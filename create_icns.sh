79c95e1d2f608e07b7c67d0e2cfb1ae7a7d011bc
Rian Hunter <rian@alum.mit.edu>
2014-01-03 17:21:59 -0800

    create_icons.sh: add 2x versions of icons for retina display

--- create_icns.sh
+++ create_icns.sh
@@ -5,11 +5,13 @@ set -e
 DIR=$(mktemp -d -t createicns)
 
 cp logo-16-color.png "$DIR/icon_16x16.png"
+cp logo-32.png "$DIR/icon_16x16@2x.png"
 cp logo-32.png "$DIR/icon_32x32.png"
-cp logo-48.png "$DIR/icon_48x48.png"
 cp logo-64.png "$DIR/icon_32x32@2x.png"
 cp logo-128.png "$DIR/icon_128x128.png"
+cp logo-256.png "$DIR/icon_128x128@2x.png"
 cp logo-256.png "$DIR/icon_256x256.png"
+cp logo-512.png "$DIR/icon_256x256@2x.png"
 cp logo-512.png "$DIR/icon_512x512.png"
 cp logo-1024.png "$DIR/icon_512x512@2x.png"
 
