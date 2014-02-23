1127635f65449f4a2b5ec0b02bc926bfeb9fbb76
Rian Hunter <rian@alum.mit.edu>
2014-02-23 01:34:10 -0800

    build system: fix safe_ramdisk.inf  to include required inf sections, add tools for driver signing

---
+++ tools/generate_signed_driver_security_catalog.bat
@@ -0,0 +1,8 @@
+echo off
+
+inf2cat /driver:. /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64
+
+rem /c is where the signing certificate (Rian Hunter) is stored
+rem /ac is our cross signing cert
+
+signtool sign /s MY /n "Rian Hunter" /ac digicert.crt /fd sha1 /t "http://timestamp.digicert.com" safe_ramdisk.cat
---
+++ tools/generate_signed_driver_security_catalog.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+exec cmd //C generate_signed_driver_security_catalog.bat
