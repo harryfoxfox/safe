6539e511714f6d33393d375cccc79ce63c5ddbba
Rian Hunter <rian@alum.mit.edu>
2014-02-23 01:50:33 -0800

    windows build: generate signed security catalog file for windows driver install

--- tools/generate_signed_driver_security_catalog.sh
+++ tools/generate_signed_driver_security_catalog.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-exec cmd //C generate_signed_driver_security_catalog.bat
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+exec cmd //C $DIR/generate_signed_driver_security_catalog.bat
