f7622bd4b15ff695db57b6c6a116e9dea9b1a1c6
Rian Hunter <rian@alum.mit.edu>
2014-03-04 19:59:15 -0800

    increment build number

--- src/safe/version.h
+++ src/safe/version.h
@@ -23,7 +23,7 @@
 // the build version number indicates bug fixes
 
 #define SAFE_VERSION_MAJOR 0
-#define SAFE_VERSION_BUILD 4
+#define SAFE_VERSION_BUILD 5
 
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
