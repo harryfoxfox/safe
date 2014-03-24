333315950e0eea1c5f9d7a47c9b2f2bde175fe50
Rian Hunter <rian@alum.mit.edu>
2014-03-24 09:39:18 +0100

    mac ui: implement showBlockingSheetMessageNoCatch(), refactor showBlockingSheetMessage() in terms of the former new function

--- src/safe/mac/SFXProgressSheetController.h
+++ src/safe/mac/SFXProgressSheetController.h
@@ -6,10 +6,12 @@
 //  Copyright (c) 2013 Rian Hunter. All rights reserved.
 //
 
+#import <safe/either.hpp>
+
 #import <Cocoa/Cocoa.h>
 
-#include <type_traits>
-#include <exception>
+#import <type_traits>
+#import <exception>
 
 @interface SFXProgressSheetController : NSWindowController
 @property (weak) IBOutlet NSTextField *msgTextField;
@@ -40,41 +42,60 @@ createAndStartProgressSheet(NSWindow *w, NSString *msg) {
     return progressSheet;
 }
 
-template<class R, class F, class ...Args>
+template<class F, class ...Args>
 // TODO: maybe enable if typename std::result_of<F(Args...)>::type is convertible to R
 void
-showBlockingSheetMessage(NSWindow *w,
-                         NSString *msg,
-                         void (^onSuccess)(R),
-                         void (^onException)(const std::exception_ptr &),
-                         F f,
-                         Args... args) {
+showBlockingSheetMessageNoCatch(NSWindow *w,
+                                NSString *msg,
+                                void (^onSuccess)(typename std::result_of<F(Args...)>::type),
+                                F f, Args... args) {
     // NB: this method would be a lot more annoying to write without ARC
     SFXProgressSheetController *progressSheet = createAndStartProgressSheet(w, msg);
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
                    ^{
-                       try {
-                           auto res = f(args...);
-                           auto resp = new decltype(res)(std::move(res));
-                           dispatch_async(dispatch_get_main_queue(),
-                                          ^{
-                                              auto res2 = std::move(*resp);
-                                              delete resp;
-                                              [NSApp endSheet:progressSheet.window];
-                                              onSuccess(std::move(res2));
-                                          });
-                       }
-                       catch (...) {
-                           auto eptr = std::current_exception();
-                           dispatch_async(dispatch_get_main_queue(),
-                                          ^{
-                                              [NSApp endSheet:progressSheet.window];
-                                              onException(eptr);
-                                          });
-                       }
+                       auto res = f(args...);
+                       auto resp = new decltype(res)(std::move(res));
+                       dispatch_async(dispatch_get_main_queue(),
+                                      ^{
+                                          auto res2 = std::move(*resp);
+                                          delete resp;
+                                          [NSApp endSheet:progressSheet.window];
+                                          onSuccess(std::move(res2));
+                                      });
                    });
-};
+}
+
+template<class F, class ...Args>
+void
+showBlockingSheetMessage(NSWindow *w,
+                         NSString *msg,
+                         void (^onSuccess)(typename std::result_of<F(Args...)>::type),
+                         void (^onException)(const std::exception_ptr &),
+                         F f,
+                         Args... args) {
+    typedef eit::either<std::exception_ptr, typename std::result_of<F(Args...)>::type> ToRunResultType;
+
+    auto toRun = ^{
+        try {
+            return ToRunResultType(f(args...));
+        }
+        catch (...) {
+            return ToRunResultType(std::current_exception());
+        }
+    };
+
+    auto onComplete = ^(ToRunResultType res) {
+        if (res.has_left()) {
+            onException(res.left());
+        }
+        else {
+            onSuccess(std::move(res.right()));
+        }
+    };
+
+    showBlockingSheetMessageNoCatch(w, msg, onComplete, toRun);
+}
 
 template<class F, class ...Args>
 void
